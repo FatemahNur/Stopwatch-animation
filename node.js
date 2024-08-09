b9dfe21e1e795388ad9b489d11ff32f92b732d08
FATIMA NOOR <fsayedno@uwo.ca>
2024-08-09 13:12:01 -0400

    first commit

---
+++ node.js
@@ -0,0 +1,40 @@
+let startTime, updatedTime, difference, tInterval;
+let running = false;
+
+function startStopwatch() {
+    if (!running) {
+        startTime = new Date().getTime() - (difference || 0);
+        tInterval = setInterval(updateTime, 1000);
+        running = true;
+    }
+}
+
+function pauseStopwatch() {
+    if (running) {
+        clearInterval(tInterval);
+        difference = new Date().getTime() - startTime;
+        running = false;
+    }
+}
+
+function resetStopwatch() {
+    clearInterval(tInterval);
+    difference = 0;
+    running = false;
+    document.getElementById('display').innerHTML = "00:00:00";
+}
+
+function updateTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+    
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    
+    document.getElementById('display').innerHTML = hours + ":" + minutes + ":" + seconds;
+}
